bb3517c33cf4dfadd7af9048e25c874028d919e6
Anton Korniychuk <dev@korniychuk.pro>
2019-06-03 21:25:56 +0300

    readme & license copied to dist

--- config/webpack.prod.js
+++ config/webpack.prod.js
@@ -1,4 +1,5 @@
 const webpackMerge = require('webpack-merge');
+const CopyPlugin = require('copy-webpack-plugin');
 
 const commonConfig = require('./webpack.common');
 
@@ -11,6 +12,21 @@ module.exports = function(options) {
   const ENV = (process.env.ENV = process.env.NODE_ENV = 'production');
 
   return webpackMerge(commonConfig({ env: ENV }), {
+    /**
+     * Add additional plugins to the compiler.
+     *
+     * See: https://webpack.js.org/configuration/plugins/
+     */
+    plugins: [
 
+      /**
+       * See: https://webpack.js.org/plugins/copy-webpack-plugin
+       */
+      new CopyPlugin([
+        { from: 'README.md',    to: '.' },
+        { from: 'package.json', to: '.' },
+        { from: 'LICENSE',      to: '.' },
+      ]),
+    ],
   });
 };
