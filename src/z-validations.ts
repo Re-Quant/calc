f81d8909d77a50a58e238635bcd789f23b666002
oleg.rovenskyi <oleg.rovenskyi@dev-pro.net>
2019-10-10 00:01:42 +0300

    validation: Add Error Interface and basic validation.

--- src/z-validations.ts
+++ src/z-validations.ts
@@ -4,9 +4,9 @@ import {
   TradeOrderArg
 } from './models';
 
-export interface ErrorInfo<T> {
+export interface ErrorInfo {
   message: string;
-  actual?: T;
+  actual?: number;
 }
 
 export type OrderError = {[key in keyof TradeOrderArg]?: ErrorInfo};
@@ -16,41 +16,49 @@ export interface TradeOrderErrors {
 }
 
 export interface IsNumberOptions {
-  allowNaN?: boolean;
-  allowInfinity?: boolean;
+  allowNaN: boolean;
+  allowInfinity: boolean;
 }
 
 export interface ValidationTradeErrors {
-  tradeType?: ErrorInfo;
-  deposit?: ErrorInfo;
-  risk?: ErrorInfo;
-  maxTradeVolumeQuoted?: ErrorInfo;
-  leverage?: {
+  tradeType: ErrorInfo;
+  deposit: ErrorInfo;
+  risk: ErrorInfo;
+  maxTradeVolumeQuoted: ErrorInfo;
+  leverage: {
     allow?: ErrorInfo;
     max?: ErrorInfo;
   };
-  breakeven?: {
+  breakeven: {
     fee?: ErrorInfo;
   };
 
-  entries?: TradeOrderErrors;
-  stops?: TradeOrderErrors;
-  takes?: TradeOrderErrors;
+  entries: TradeOrderErrors;
+  stops: TradeOrderErrors;
+  takes: TradeOrderErrors;
 }
 
 class ZValidationErrorFactory {
-  public createErrorInfo(
-    model: ValidationTradeErrors = {},
-    path: string,
-    value: ErrorInfo | TradeOrderErrors,
-  ): ValidationTradeErrors {
-    let model = model;
-    const pathParts = path.split('.');
-    let currentObject = model;
+  public createErrorInfo<O extends ValidationTradeErrors, K1 extends keyof O>(
+    keys: [K1],
+    value: ErrorInfo,
+    model: O,
+  ): O[K1];
+  public createErrorInfo<O extends ValidationTradeErrors, K1 extends keyof O, K2 extends keyof O[K1]>(
+    keys: [K1, K2],
+    value: ErrorInfo,
+    model: O,
+  ): O[K1][K2];
+  public createErrorInfo<O extends ValidationTradeErrors>(
+    keys: string[],
+    value: ErrorInfo,
+    model: O,
+  ): any {
+    let currentObject: any = model;
 
-    for (let i in pathParts) {
-      const key = pathParts[i];
-      const length = pathParts.length - 1;
+    for (let i in keys) {
+      const key = keys[i];
+      const length = keys.length - 1;
 
       if (+i === length) {
         currentObject[key] = value;
@@ -79,7 +87,7 @@ class ZValidations {
   }
   public zErrorFactory: ZValidationErrorFactory;
 
-  private errors: ValidationTradeErrors = {};
+  private errors: ValidationTradeErrors = {} as any;
 
   constructor() {
     this.zErrorFactory = new ZValidationErrorFactory();
@@ -143,97 +151,153 @@ class ZValidations {
   public validateCommonFields(p: TradeInfoArgs): ValidationTradeErrors | void {
     // deposit
     if (!p.deposit && !this.isDefined(p.deposit)) {
-      this.zErrorFactory.createErrorInfo(this.errors, 'deposit', {
-        message: this.messages.required,
-      });
+      this.zErrorFactory.createErrorInfo(
+        ['deposit'],
+        {
+          message: this.messages.required,
+        },
+        this.errors
+      );
     }
 
     if (p.deposit && !this.isNumber(p.deposit)) {
-      this.zErrorFactory.createErrorInfo(this.errors, 'deposit', {
-        message: this.messages.number,
-      });
+      this.zErrorFactory.createErrorInfo(
+        ['deposit'],
+        {
+          message: this.messages.number,
+        },
+        this.errors
+      );
     }
 
     if (p.deposit && this.isNumber(p.deposit) && !this.min(p.deposit, 0)) {
-      this.zErrorFactory.createErrorInfo(this.errors, 'deposit', {
-        message: this.messages.minValue(p.deposit),
-        actual: p.deposit,
-      });
+      this.zErrorFactory.createErrorInfo(
+        ['deposit'],
+        {
+          message: this.messages.minValue(p.deposit),
+          actual: p.deposit,
+        },
+        this.errors
+      );
     }
 
     // risk
     if (!p.risk && !this.isDefined(p.risk)) {
-      this.zErrorFactory.createErrorInfo(this.errors, 'risk', {
-        message: this.messages.required,
-      });
+      this.zErrorFactory.createErrorInfo(
+        ['risk'],
+        {
+          message: this.messages.required,
+        },
+        this.errors
+      );
     }
 
     if (p.risk && !this.isNumber(p.risk)) {
-      this.zErrorFactory.createErrorInfo(this.errors, 'risk', {
-        message: this.messages.number,
-      });
+      this.zErrorFactory.createErrorInfo(
+        ['risk'],
+        {
+          message: this.messages.number,
+        },
+        this.errors
+      );
     }
 
     if (p.risk && this.isNumber(p.risk) && !this.min(p.risk, 0)) {
-      this.zErrorFactory.createErrorInfo(this.errors, 'risk', {
-        message: this.messages.minValue(p.risk),
-        actual: p.risk,
-      });
+      this.zErrorFactory.createErrorInfo(
+        ['risk'],
+        {
+          message: this.messages.minValue(p.risk),
+          actual: p.risk,
+        },
+        this.errors
+      );
     }
 
     if (p.risk && this.isNumber(p.risk) && !this.max(p.risk, 1)) {
-      this.zErrorFactory.createErrorInfo(this.errors, 'risk', {
-        message: this.messages.maxValue(p.risk),
-        actual: p.risk,
-      });
+      this.zErrorFactory.createErrorInfo(
+        ['risk'],
+        {
+          message: this.messages.maxValue(p.risk),
+          actual: p.risk,
+        },
+        this.errors
+      );
     }
 
     // maxTradeVolumeQuoted
     if (!p.maxTradeVolumeQuoted && !this.isDefined(p.maxTradeVolumeQuoted)) {
-      this.zErrorFactory.createErrorInfo(this.errors, 'maxTradeVolumeQuoted', {
-        message: this.messages.required,
-      });
+      this.zErrorFactory.createErrorInfo(
+        ['maxTradeVolumeQuoted'],
+        {
+          message: this.messages.required,
+        },
+        this.errors
+      );
     }
 
     if (p.maxTradeVolumeQuoted && !this.isNumber(p.maxTradeVolumeQuoted)) {
-      this.zErrorFactory.createErrorInfo(this.errors, 'maxTradeVolumeQuoted', {
-        message: this.messages.number,
-      });
+      this.zErrorFactory.createErrorInfo(
+        ['maxTradeVolumeQuoted'],
+        {
+          message: this.messages.number,
+        },
+        this.errors
+      );
     }
 
     if (p.maxTradeVolumeQuoted
       && this.isNumber(p.maxTradeVolumeQuoted)
       && !this.min(p.maxTradeVolumeQuoted, 0)
     ) {
-      this.zErrorFactory.createErrorInfo(this.errors, 'maxTradeVolumeQuoted', {
-        message: this.messages.minValue(p.risk),
-        actual: p.maxTradeVolumeQuoted,
-      });
+      this.zErrorFactory.createErrorInfo(
+        ['maxTradeVolumeQuoted'],
+        {
+          message: this.messages.minValue(p.risk),
+          actual: p.maxTradeVolumeQuoted,
+        },
+        this.errors
+      );
     }
 
     // leverage
     if (!p.leverage || !p.leverage.allow) {
-      this.zErrorFactory.createErrorInfo(this.errors, 'leverage.allow', {
-        message: this.messages.required,
-      });
+      this.zErrorFactory.createErrorInfo(
+        ['leverage', 'allow'],
+        {
+          message: this.messages.required,
+        },
+        this.errors
+      );
     }
 
     if (p.leverage && p.leverage.allow && !this.isBoolean(p.leverage.allow)) {
-      this.zErrorFactory.createErrorInfo(this.errors, 'leverage.allow', {
-        message: this.messages.boolean,
-      });
+      this.zErrorFactory.createErrorInfo(
+        ['leverage', 'allow'],
+        {
+          message: this.messages.boolean,
+        },
+        this.errors
+      );
     }
 
     if (!p.leverage || !p.leverage.max) {
-      this.zErrorFactory.createErrorInfo(this.errors, 'leverage.max', {
-        message: this.messages.required,
-      });
+      this.zErrorFactory.createErrorInfo(
+        ['leverage', 'max'],
+        {
+          message: this.messages.required,
+        },
+        this.errors
+      );
     }
 
     if (p.leverage && p.leverage.max && !this.isNumber(p.leverage.max)) {
-      this.zErrorFactory.createErrorInfo(this.errors, 'leverage.max', {
-        message: this.messages.number,
-      });
+      this.zErrorFactory.createErrorInfo(
+        ['leverage', 'max'],
+        {
+          message: this.messages.number,
+        },
+        this.errors
+      );
     }
 
     if (p.leverage
@@ -241,10 +305,14 @@ class ZValidations {
       && this.isNumber(p.leverage.max)
       && !this.min(p.leverage.max, 0)
     ) {
-      this.zErrorFactory.createErrorInfo(this.errors, 'leverage.max', {
-        message: this.messages.minValue(leverage.max),
-        actual: p.leverage.max,
-      });
+      this.zErrorFactory.createErrorInfo(
+        ['leverage', 'max'],
+        {
+          message: this.messages.minValue(p.leverage.max),
+          actual: p.leverage.max,
+        },
+        this.errors
+      );
     }
 
     if (p.leverage
@@ -252,36 +320,56 @@ class ZValidations {
       && this.isNumber(p.leverage.max)
       && !this.max(p.leverage.max, 1000)
     ) {
-      this.zErrorFactory.createErrorInfo(this.errors, 'leverage.max', {
-        message: this.messages.maxValue(leverage.max),
-        actual: p.leverage.max,
-      });
+      this.zErrorFactory.createErrorInfo(
+        ['leverage', 'max'],
+        {
+          message: this.messages.maxValue(p.leverage.max),
+          actual: p.leverage.max,
+        },
+        this.errors
+      );
     }
 
     // tradeType
     if (!p.tradeType) {
-      this.zErrorFactory.createErrorInfo(this.errors, 'tradeType', {
-        message: this.messages.required,
-      });
+      this.zErrorFactory.createErrorInfo(
+        ['tradeType'],
+        {
+          message: this.messages.required,
+        },
+        this.errors
+      );
     }
 
     if (p.tradeType && !this.isString(p.tradeType)) {
-      this.zErrorFactory.createErrorInfo(this.errors, 'tradeType', {
-        message: this.messages.string,
-      });
+      this.zErrorFactory.createErrorInfo(
+        ['tradeType'],
+        {
+          message: this.messages.string,
+        },
+        this.errors
+      );
     }
 
     // breakeven
     if (!p.breakeven || !p.breakeven.fee) {
-      this.zErrorFactory.createErrorInfo(this.errors, 'breakeven.fee', {
-        message: this.messages.required,
-      });
+      this.zErrorFactory.createErrorInfo(
+        ['breakeven', 'fee'],
+        {
+          message: this.messages.required,
+        },
+        this.errors
+      );
     }
 
     if (p.breakeven && p.breakeven.fee && !this.isNumber(p.breakeven.fee)) {
-      this.zErrorFactory.createErrorInfo(this.errors, 'breakeven.fee', {
-        message: this.messages.number,
-      });
+      this.zErrorFactory.createErrorInfo(
+        ['breakeven', 'fee'],
+        {
+          message: this.messages.number,
+        },
+        this.errors
+      );
     }
 
     return Object.entries(this.errors).length === 0 && this.errors.constructor === Object
@@ -301,8 +389,8 @@ class ZValidations {
   }
 
   private cleanErrors(): void {
-    this.errors = {};
+    this.errors = {} as any;
   }
 }
 
-export const ZValidations = new ZValidations();
+export const zValidations = new ZValidations();
