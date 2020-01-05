167775297bea3cf0975465d922a704c2206c932a
oleg.rovenskyi <oleg.rovenskyi@dev-pro.net>
2020-01-05 18:55:42 +0200

    validation: unit test for risk field

--- src/z-validations.spec.ts
+++ src/z-validations.spec.ts
@@ -130,7 +130,7 @@ describe('ZValidations', () => {
           };
           const riskRangeValidValue = [0, 0.01, 0.05, 0.1];
 
-          riskRangeValidValue.forEach((value) => {
+          riskRangeValidValue.forEach((value): void => {
             testData.risk = value;
             expect(zValidationsMock.validate(testData)).to.equal(undefined);
           });
